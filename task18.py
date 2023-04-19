532f038d1555da6bb27fcb5dc8aa41d5aa7adaa7
ct <wadim_fo@mail.ru>
2023-04-19 16:56:19 +0300

    добавление решение для задачи 18

--- task18.py
+++ task18.py
@@ -7,3 +7,20 @@
 #     1 2 3 4 5
 #     6
 #     -> 5
+
+import random
+
+def find_element(a: int, b: list):
+    minimum = abs(a-b[0])
+    result = b[0]
+    for i in range(1, len(b)):
+        current = abs(a - b[i])
+        if current < minimum:
+            minimum = current
+            result = b[i]
+    return result
+generator_list = int(input('Введите размер строки: '))
+random_list = [random.randint(0, 25) for i in range(generator_list)]
+print(f'Имеем полученный список {random_list}')
+search_number = int(input('Введите число для поиска: '))
+print(f'Самое ближайшее число к {search_number} в списке {find_element(search_number, random_list)}')
